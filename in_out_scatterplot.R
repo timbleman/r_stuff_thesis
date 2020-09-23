147d01ec32c0ab9a780f69be328f94aba3be5416
Tim Fraunholz <fraunholz.t@web.de>
2020-09-23 12:26:44 +0200

    basic in out scatterplot without distribuition matching

--- in_out_scatterplot.R
+++ in_out_scatterplot.R
@@ -1,11 +1,28 @@
 # scatterplot of input and output similarities for one test
 
-setwd("C:/CS1_R-Intro/experiments-beamng-ai-wo-minlen-wo-infspeed-7-steering-4-len-20200821T084856Z-001")
+# without OBEs
+setwd("C:/CS1_R-Intro/experiments-driver-ai-no-obe-wo-minlen-wo-infspeed")
+#setwd("C:/CS1_R-Intro/experiments-beamng-ai-no-obe-wo-minlen-wo-infspeed")
 
-input_name <- "jaccard_60alph.csv"
-output_name <- "steering_speed_dist.csv"
+input_name <- "jaccard_44alph.csv"
+output_name <- "steering_speed_dist_binary.csv"
 
-centerpoint <- "no"
+# select a centerpoint, if it is not in the matrix, a random one is chose
+centerpoint <- "random--la1021"
 
 inm <- read.csv(input_name, check.names=FALSE, row.names=1)
 outm <- read.csv(output_name, check.names=FALSE, row.names=1)
+
+stopifnot(names(inm) == names(outm))
+
+# set.seed(1234)
+if (!centerpoint %in% names(inm)){
+	ran_num <- sample(1:length(inm), 1)
+	centerpoint <- names(inm)[ran_num]
+}
+
+print(paste("Selected", centerpoint, "as centerpoint."))
+
+input_sim <- inm[,centerpoint]  # without comma to retrieve 1d-dframe with names
+output_sim <- outm[,centerpoint]
+plot(input_sim, output_sim, xlim=c(1,0), ylim=c(1,0))
