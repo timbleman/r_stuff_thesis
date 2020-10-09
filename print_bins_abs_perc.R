ccb1f0642ba035d28f81f342b38a0271fa0c7655
Tim Fraunholz <fraunholz.t@web.de>
2020-10-09 20:24:50 +0200

    added scaling and labeling for 1d plot

--- print_bins_abs_perc.R
+++ print_bins_abs_perc.R
@@ -5,12 +5,19 @@ setwd("C:/CS1_R-Intro/driver-ai-wo-minlen-wo-infspeed-7-steering-4-len-20200818T
 # these have to be adjusted by the user
 bin_metric <- "speed_steering_2d_bins.csv"
 #bin_metric <- "steering_bins.csv"
-#bin_metric <- "speed_bins.csv"
+bin_metric <- "speed_bins.csv"
 as_percentage <- FALSE
 # These have to be correctly entered!!!
 x_dim = 16
-y_dim = 16
-selected_test <- "random"#"one-plus-o1011" #"random"
+y_dim = 1
+selected_test <- "one-plus-o1011" #"random"
+# TODO non equal distr for drvr/bngai
+EQUAL_X_AXS <- TRUE
+if(EQUAL_X_AXS){
+	predef_col_labels <- round(seq(-1, 1, 2/x_dim-1), digits=2)
+} else {
+	predef_col_labels <- rep(0, x_dim)
+}
 
 all_bins <- read.csv(bin_metric, check.names=FALSE, row.names=1)
 
@@ -34,6 +41,7 @@ if (as_percentage){
 }
 #all_bins[selected_test,] <- format(round(all_bins[selected_test,], 2), nsmall = 2)
 print_bins <- as.numeric(as.vector(all_bins[selected_test,]))
+# x strangely is representing rows not columns
 image_bins <- matrix(print_bins, nrow=y_dim, ncol=x_dim)
 print_bins <- format(round(print_bins, 2), nsmall = 2)
 print_bins <- matrix(print_bins, nrow=y_dim, ncol=x_dim)
@@ -49,8 +57,23 @@ for (i in 1:y_dim){
 }
 horiz_line
 
+# stupid image function needs rotation
+rotate <- function(x) t(apply(x, 2, rev))
+rotate_1d <- function(x){
+	return(matrix(x[1,], nrow=x_dim, ncol=1))
+}
+
+
+dev.off()
 if (y_dim == 1){
-	image(image_bins, ylab=paste(bin_metric), axes=FALSE)
+	dev.new(width=x_dim/2, height=2.3)
+	rotated <- rotate_1d(image_bins)
+	#
+	rownames(rotated) <- predef_col_labels
+	image(rotated, ylab=paste(bin_metric), yaxt='n', xaxt='n')
+	#axis( 2, at=seq(0,1,length.out=ncol(rotated) ), labels= colnames(rotated), las= 2 )
+	axis( 1, at=seq(0,1,length.out=nrow(rotated) ), labels= rownames(rotated), las= 2)
 } else {
-	image(image_bins, ylab=paste(bin_metric), axes=FALSE)
+	dev.new(width=x_dim, height=y_dim)
+	image(rotate(image_bins), ylab=paste(bin_metric), axes=FALSE)
 }
