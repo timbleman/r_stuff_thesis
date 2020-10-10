f014a42f87a3aeeb219166510895dbfadc901c0e
Tim Fraunholz <fraunholz.t@web.de>
2020-10-10 16:19:08 +0200

    added fitting axis

--- print_bins_abs_perc.R
+++ print_bins_abs_perc.R
@@ -1,23 +1,41 @@
 # print bins absolute or as percentage for a test
+library(fields)
 
 setwd("C:/CS1_R-Intro/driver-ai-wo-minlen-wo-infspeed-7-steering-4-len-20200818T120651Z-001")
 
-# these have to be adjusted by the user
-bin_metric <- "speed_steering_2d_bins.csv"
-#bin_metric <- "steering_bins.csv"
-bin_metric <- "speed_bins.csv"
+dimensions <- 2
+st_or_sp <- "sp"
+if(dimensions == 2){ 
+	# these have to be adjusted by the user
+	bin_metric <- "speed_steering_2d_bins.csv"
+	bin_metric <- "speed_steering_2d_bins_adjusted.csv"
+}
+if(dimensions == 1){ 
+	if(st_or_sp == "st") {
+		bin_metric <- "steering_bins.csv"
+	}
+	if(st_or_sp == "sp") {
+		bin_metric <- "speed_bins.csv"
+	}
+}
 as_percentage <- FALSE
 # These have to be correctly entered!!!
 x_dim = 16
-y_dim = 1
+if(dimensions == 1){ 
+	y_dim = 1
+} else {
+	y_dim = 16
+}
+NUM_COL <- 64
 selected_test <- "one-plus-o1011" #"random"
 # TODO non equal distr for drvr/bngai
 EQUAL_X_AXS <- TRUE
 if(EQUAL_X_AXS){
-	predef_col_labels <- round(seq(-1, 1, 2/x_dim-1), digits=2)
+	predef_col_labels <- round(seq(-1, 1, 2/(x_dim-1)), digits=2)
 } else {
 	predef_col_labels <- rep(0, x_dim)
 }
+predef_speed_labels <- round(seq(0, 85, 85/(y_dim-1)), digits=2)
 
 all_bins <- read.csv(bin_metric, check.names=FALSE, row.names=1)
 
@@ -70,10 +88,23 @@ if (y_dim == 1){
 	rotated <- rotate_1d(image_bins)
 	#
 	rownames(rotated) <- predef_col_labels
-	image(rotated, ylab=paste(bin_metric), yaxt='n', xaxt='n')
 	#axis( 2, at=seq(0,1,length.out=ncol(rotated) ), labels= colnames(rotated), las= 2 )
-	axis( 1, at=seq(0,1,length.out=nrow(rotated) ), labels= rownames(rotated), las= 2)
+	if(st_or_sp == "st"){
+		fields::image.plot(rotated, xlab="steering bins", yaxt='n', xaxt='n', col=heat.colors(NUM_COL))
+		#axis( 2, at=seq(0,1,length.out=ncol(rotated) ), labels= colnames(rotated), las= 2 )
+		axis( 1, at=seq(0,1,length.out=nrow(rotated) ), labels= rownames(rotated), las= 2)
+	} else if(st_or_sp == "sp"){
+		fields::image.plot(rotated, xlab="speed bins", yaxt='n', xaxt='n', col=heat.colors(NUM_COL))
+		#axis( 2, at=seq(0,1,length.out=ncol(rotated) ), labels= colnames(rotated), las= 2 )
+		axis( 1, at=seq(0,1,length.out=nrow(rotated) ), labels= rownames(rotated), las= 2)
+	}
 } else {
 	dev.new(width=x_dim, height=y_dim)
-	image(rotate(image_bins), ylab=paste(bin_metric), axes=FALSE)
+	rotated <- rotate(image_bins)
+	rownames(rotated) <- predef_col_labels
+	colnames(rotated) <- predef_speed_labels
+	fields::image.plot(rotated, xlab="speed bins", ylab="steering bins", axes=FALSE, col=heat.colors(NUM_COL))
+	axis( 2, at=seq(0,1,length.out=nrow(rotated) ), labels= rownames(rotated), las= 2)
+	axis( 1, at=seq(0,1,length.out=ncol(rotated) ), labels= colnames(rotated), las= 2 )
 }
+title(main=paste(bin_metric))
