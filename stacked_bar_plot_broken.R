50c0954f16d307486b87a5a78e3af6d725811b06
Tim Fraunholz <fraunholz.t@web.de>
2020-11-16 16:09:43 +0100

    text size for stacked bar plots

--- stacked_bar_plot_broken.R
+++ stacked_bar_plot_broken.R
@@ -1,6 +1,6 @@
 # plot for the broken tests in both datasets
 
-setwd("C:/CS1_R-Intro/)
+setwd("C:/CS1_R-Intro/")
 
 library(ggplot2)
 library(hrbrthemes)
@@ -22,10 +22,12 @@ dframe_broken_execs <- data.frame(d_sets, class, count)
 cols <- c(valid_execution="#66CC00", broken_speed="brown3", 
 		other="cadetblue3", under_minimum_length="#FFFF33")
 
+font_size <- 18
 ggplot(dframe_broken_execs, aes(fill=class, x=d_sets, y=count)) +
 	geom_bar(position="stack", stat="identity") + 
 	ggtitle("Classes of executions") + 
-	theme_ipsum(axis_text_size=10, axis_title_size = 11) + 
-	xlab("datasets") + 
-	ylab("test executions") + 
-	scale_fill_manual(values = cols)
+	theme_ipsum(axis_text_size=font_size, axis_title_size=font_size) + 
+	xlab("Datasets") + 
+	ylab("Test executions") + 
+	scale_fill_manual(values = cols) + 
+	theme(text = element_text(size=font_size)) # scale the legend
--- stacked_bar_plot_datasets.R
+++ stacked_bar_plot_datasets.R
@@ -1,9 +1,10 @@
-setwd("C:/CS1_R-Intro/)
+# plot for the number of OBEs in BeamNG/DriverAI Datasets
+# numbers have been collected manually
+
+setwd("C:/CS1_R-Intro/")
 
 library(ggplot2)
 library(hrbrthemes)
-
-# plot for the number of OBEs in BeamNG/DriverAI Datasets
 
 OBE = "OBE"
 NONOBE = "non_OBE"
@@ -14,7 +15,7 @@ obe_vs_non_BNG_full <- c(rep(OBE, 83), rep(NONOBE, 86))
 obe_vs_non_BNG_trimmed <- c(rep(OBE, 56), rep(NONOBE, 59))
 obe_vs_non_DRVR_full <- c(rep(OBE, 28), rep(NONOBE, 438))
 obe_vs_non_DRVR_trimmed <- c(rep(OBE, 18), rep(NONOBE, 273))
-obe <- c(obe_vs_non_BNG_full, obe_vs_non_BNG_trimmed, obe_vs_non_DRVR_full,
+Test_outcome <- c(obe_vs_non_BNG_full, obe_vs_non_BNG_trimmed, obe_vs_non_DRVR_full,
 		obe_vs_non_DRVR_trimmed)
 
 count <- rep(1, 1041)
@@ -22,10 +23,13 @@ dframe_trimmed_vs_not <- data.frame(d_sets, obe, count)
 
 cols <- c(OBE="brown3", non_OBE="cadetblue3")
 
-ggplot(dframe_trimmed_vs_not, aes(fill=obe, x=d_sets, y=count)) +
+font_size <- 18
+ggplot(dframe_trimmed_vs_not, aes(fill=Test_outcome, x=d_sets, y=count)) +
 	geom_bar(position="stack", stat="identity") + 
 	ggtitle("OBE distribution in the datasets") + 
-	theme_ipsum(axis_text_size=10, axis_title_size = 11) + 
-	xlab("datasets") + 
-	ylab("test executions") + 
-	scale_fill_manual(values = cols)
+	theme_ipsum(axis_text_size=font_size, axis_title_size = font_size) + 
+	xlab("Datasets") + 
+	ylab("Test executions") + 
+	scale_x_discrete(guide = guide_axis(n.dodge=3)) +  # split the x labels in two lines
+	scale_fill_manual(values = cols) + 
+	theme(text = element_text(size=font_size)) # scale the legend
