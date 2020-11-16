68b844aa4dcdd0a7e9be33dab45a6a28eec590f4
Tim Fraunholz <fraunholz.t@web.de>
2020-11-16 11:22:46 +0100

    changed text size for alph time plot

--- alphabet_times_comp.R
+++ alphabet_times_comp.R
@@ -7,13 +7,8 @@ Jaccard_comp_times <- c("28" = 4.79, "44" = 5.34, "60" = 6.07, " "=0)
 SDL_2D_comp_times <- c("28" = 55.34, "44" = 57.25, "60" = 62.59, " "=0)
 CUR_SDL_comp_times <- c("7" = 26.93, "11" = 28.71, "15" =31.04)
 
-#df <- data.frame(Jaccard_comp_times, SDL_2D_comp_times, CUR_SDL_comp_times)
-#names(df) <- c("jaccard_28", "jaccard_44", "jaccard_60")
 
 vec <- c(Jaccard_comp_times, SDL_2D_comp_times, CUR_SDL_comp_times)
-#vec <- c("jaccard_28" = Jaccard_comp_times,
-#		"jaccard_44" = SDL_2D_comp_times, 
-#		"jaccard_60"=CUR_SDL_comp_times)
 
 # ugly and hacky, lol
 first_col <- "#FEEB65"
@@ -23,6 +18,13 @@ cols <- c(first_col, second_col, third_col, "#00FF99",
           first_col, second_col, third_col, "#00FF99",
           first_col, second_col, third_col, "#00FF99")
 # ugly and hacky, lol
-names = "Jaccard                     Sliding window SDL_2D        Sliding window CUR_SDL"
+names = "Jaccard                  sw_sdl_2d               sw_cur_sdl"
 
-barplot(vec, ylab="time in seconds", col=cols, xlab=names, border=par(lty=0))
+par(mar=c(5,5,4,1)+.1)  # margins bottom, left, top, right
+
+font_mult <- 2
+
+# what does this one do: "border=par(lty=0)," ? Produces only a warning...
+barplot(vec, ylab="Time in seconds", col=cols, xlab=names, 
+		cex.axis = font_mult, cex.lab = font_mult, cex.main = font_mult,
+		cex.names = font_mult)
