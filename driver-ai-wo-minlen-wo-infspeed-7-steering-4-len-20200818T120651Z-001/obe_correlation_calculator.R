58b25997eca05ff1ceac7347bcabe58d5e22a6b3
Tim Fraunholz <fraunholz.t@web.de>
2020-12-31 14:58:53 +0100

    comments correlation

--- driver-ai-wo-minlen-wo-infspeed-7-steering-4-len-20200818T120651Z-001/obe_correlation_calculator.R
+++ driver-ai-wo-minlen-wo-infspeed-7-steering-4-len-20200818T120651Z-001/obe_correlation_calculator.R
@@ -1,16 +1,13 @@
-# compute correlation between test obe behavior to similarity
-
-# jo, jitter makes everthing a bit more readable, amny dont have near tests
-# plot(jitter(distances, factor=0.1), jitter(for_each_num_obes[,"num_obes"], factor=0.1))
+# Compute correlation between test obe behavior to similarity
+# Things to adjust:
+# setwd() to the right suite, uncomment BeamNG.AI or DriverAI
+# Add (input) metrics to input_names_to_load, comment and uncomment ones
 
 # path to tests
+# BeamNG.AI
 setwd("C:/CS1_R-Intro/experiments-beamng-ai-wo-minlen-wo-infspeed-7-steering-4-len-20200821T084856Z-001")
-# setwd("C:/CS1_R-Intro/driver-ai-wo-minlen-wo-infspeed-7-steering-4-len-20200818T120651Z-001")
+# DriverAI
 #setwd("C:/CS1_R-Intro/driver-ai-wo-minlen-wo-infspeed-7-steering-4-len-20200818T120651Z-001")
-
-for_each_num_obes <- read.csv("for_each_num_obes.csv")
-
-tests_that_fail <- for_each_num_obes[for_each_num_obes$num_obes == 1, ]
 
 # comment or uncomment these for desired metrics
 input_names_to_load <- list("jaccard_7ang_4len.csv" = NA, 
@@ -31,6 +28,11 @@ input_names_to_load <- list ("cursdl_lcs_7ang.csv" = NA,
 					"cursdl_3_lcstr_7ang.csv" = NA,
 					"cursdl_5_lcstr_7ang.csv" = NA)
 
+PLOTTING = TRUE
+
+# Get tests that fail
+for_each_num_obes <- read.csv("for_each_num_obes.csv")
+tests_that_fail <- for_each_num_obes[for_each_num_obes$num_obes == 1, ]
 
 # Load all the csvs for the names 
 for (name in names(input_names_to_load)){	
@@ -49,28 +51,19 @@ for (in_dist_name in names(input_names_to_load)){
 	print(in_dist_name)
 	in_dist = input_names_to_load[[in_dist_name]]
 	
-
-
-
-	#jaccard_matrix = read.csv("jaccard.csv", check.names=FALSE)
-
 	names_of_tests_that_fail = tests_that_fail$test
 	sum_cors <- 0
 	sum_p_vals <- 0
 	for (name in names_of_tests_that_fail){
 		distances <- in_dist[[name]]
-		#print(class(jaccard_matrix[[name]]))
 		num_obes <- for_each_num_obes[["num_obes"]]
 		cor_output <- cor.test(distances, num_obes, method="spearman")
 		
 		estimated_rho = as.numeric(cor_output[["estimate"]])
 		sum_cors <- sum_cors + estimated_rho
-		#print(cor_output)
 		p_val = cor_output[["p.value"]]
-		#print(p_val)
 		sum_p_vals <- sum_p_vals + p_val
 	}
-	# TODO does the - make sense?
 	avg_current <- sum_cors/as.numeric(length(names_of_tests_that_fail))
 	avgs[in_dist_name] <- avg_current
 	print(paste("Average obe correlation for ", in_dist_name, avg_current))
@@ -80,12 +73,17 @@ for (in_dist_name in names(input_names_to_load)){
 	i <- i + 1
 }
 
+# jitter makes everthing a bit more readable, many dont have near tests
+# plot(jitter(distances, factor=0.1), jitter(for_each_num_obes[,"num_obes"], factor=0.1))
+
 names(avgs) <- new_names
 avgs
-# adjusts margin to fit the whole names
-op <- par(mar=c(13,4,4,2))
-barplot(avgs, las=2)   # , main="BeamNGAI OBE correlation")   # add title
-remove(op)                  
+if (PLOTTING){
+  # adjusts margin to fit the whole names
+  op <- par(mar=c(13,4,4,2))
+  barplot(avgs, las=2)   # , main="BeamNGAI OBE correlation")   # add title
+  remove(op)                  
+}
 
 # stupid way of commenting, but presented in the course
 if(FALSE){
